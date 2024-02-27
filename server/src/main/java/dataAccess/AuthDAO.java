b8201299ccb2b1e190ffc024ec60b220fe0dc136
Andres Herrera <andresherrera1142@gmail.com>
2024-02-27 00:54:09 -0700

    Implemented(Phase 3): Started Data Access implementations: GameDAO, AuthDAO, and UserDAO.

--- server/src/main/java/dataAccess/AuthDAO.java
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -1,6 +1,37 @@
 package dataAccess;
 
+import model.AuthData;
+
+import java.util.HashMap;
+
 public class AuthDAO implements DataAccess {
+    private final HashMap<String, AuthData> authTokens = new HashMap<>();
 
+    public void clear () {
+        authTokens.clear();
+    }
 
+    public AuthData createAuth (String authToken, String username) throws DataAccessException {
+        if (authTokens.containsKey(authToken)) {
+            throw new DataAccessException("Auth token already exists");
+        }
+        AuthData newAuth = new AuthData(authToken, username);
+        authTokens.put(authToken, newAuth);
+        return newAuth;
+    }
+
+    public AuthData getAuth (String authToken) throws DataAccessException {
+        AuthData auth = authTokens.get(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Auth token not found");
+        }
+        return auth;
+    }
+
+    public void deleteAuth (String authToken) throws DataAccessException {
+        if (!authTokens.containsKey(authToken)) {
+            throw new DataAccessException("Auth token not found");
+        }
+        authTokens.remove(authToken);
+    }
 }
--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,6 +1,47 @@
 package dataAccess;
 
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
 public class GameDAO implements DataAccess {
+    private int nextGameID = 1;
+    private final HashMap<Integer, GameData> games = new HashMap<>();
 
+    public void clear () {
+        games.clear();
+        nextGameID = 1;
+    }
 
+    public GameData createGame (String gameName) {
+        int gameID = nextGameID++;
+        GameData newGame = new GameData(gameID, null, null, gameName, games.get(gameID));
+        games.put(gameID, newGame);
+        return newGame;
+    }
+
+    public GameData getGame (int gameID) {
+        return games.get(gameID);
+    }
+
+    public GameData updateGame (GameData updatedGame) throws DataAccessException {
+        int gameID = updatedGame.getGameID();
+        if (!games.containsKey(gameID)) {
+            throw new DataAccessException("Game not found");
+        }
+        games.put(gameID, updatedGame);
+        return updatedGame;
+    }
+
+    public Collection<GameData> listGames () {
+        return games.values();
+    }
+
+    public void deleteGame (int gameID) throws DataAccessException {
+        if (!games.containsKey(gameID)) {
+            throw new DataAccessException("Game not found");
+        }
+        games.remove(gameID);
+    }
 }
