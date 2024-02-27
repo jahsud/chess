24d76e5439cc62af781f69ecec50ef9374b46be7
Andres Herrera <andresherrera1142@gmail.com>
2024-02-27 00:52:03 -0700

    Implemented(Phase 3): Created Data Model Classes Records.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,4 +1,17 @@
 package model;
 
-public record AuthData(String username, String password, String email) {
+import com.google.gson.Gson;
+
+public record AuthData(String authToken, String username) {
+    public AuthData setAuthToken (String authToken) {
+        return new AuthData(authToken, this.username);
+    }
+
+    public AuthData setUsername (String username) {
+        return new AuthData(this.authToken, username);
+    }
+
+    public String toString () {
+        return new Gson().toJson(this);
+    }
 }
