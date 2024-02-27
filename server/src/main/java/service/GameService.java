e0decc45a50eb9c6236f362f38ece9d93c8cba34
Andres Herrera <andresherrera1142@gmail.com>
2024-02-27 15:28:33 -0700

    Implemented(Phase3): Restructured Dat Access Classes

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,37 +1,36 @@
 package service;
 
 import dataAccess.DataAccessException;
-import dataAccess.GameDAO;
 import dataAccess.MemoryGameDAO;
 import model.GameData;
 
 import java.util.Collection;
 
 public class GameService {
-    private final GameDAO gameDAO;
+    private final MemoryGameDAO gameDAO;
 
-    public GameService (GameDAO gameDAO) throws DataAccessException {
-        this.gameDAO = new MemoryGameDAO();
+    public GameService (MemoryGameDAO gameDAO) throws DataAccessException {
+        this.gameDAO = gameDAO;
     }
 
     public void clear () throws DataAccessException {
-        MemoryGameDAO.clear();
+        gameDAO.clear();
     }
 
     public GameData createGame (String gameName) throws DataAccessException {
-        return MemoryGameDAO.createGame(gameName);
+        return gameDAO.createGame(gameName);
     }
 
     public GameData getGame (int gameID) throws DataAccessException {
-        return MemoryGameDAO.getGame(gameID);
+        return gameDAO.getGame(gameID);
     }
 
-    public GameData updateGame (int gameID) throws DataAccessException {
-        return MemoryGameDAO.updateGame(gameID);
+    public GameData updateGame (int gameID, String gameName) throws DataAccessException {
+        return gameDAO.updateGame(gameID, gameName);
     }
 
     public Collection<GameData> listGames () throws DataAccessException {
-        return MemoryGameDAO.listGames();
+        return gameDAO.listGames();
     }
 
 }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,20 +1,42 @@
 package service;
 
-import dataAccess.AuthDAO;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryUserDAO;
 import dataAccess.DataAccessException;
+import model.AuthData;
 import model.UserData;
 
 public class UserService {
+    private final MemoryUserDAO userDAO;
+    private final MemoryAuthDAO authDAO;
 
-    private final AuthDAO authDAO;
-
-    public UserService (AuthDAO authDAO) throws DataAccessException {
+    public UserService (MemoryUserDAO userDAO, MemoryAuthDAO authDAO) {
+        this.userDAO = userDAO;
         this.authDAO = authDAO;
     }
 
-    public UserData register (UserData user) throws DataAccessException {
-        return null;
+    public AuthData register (UserData userData) throws DataAccessException {
+        if (userDAO.getUser(userData.username()) != null) {
+            throw new DataAccessException("Username already exists");
+        }
+        UserData newUser = userDAO.createUser(userData.username(), userData.password(), userData.email());
+        return new AuthData(newUser.username(), newUser.email());
     }
 
+    public AuthData login (UserData userData) throws DataAccessException {
+        UserData existingUser = userDAO.getUser(userData.username());
+        if (!existingUser.password().equals(userData.password())) {
+            throw new DataAccessException("Invalid password");
+        }
+        return new AuthData(existingUser.username(), existingUser.email());
+    }
+
+    public void logout (UserData userData) throws DataAccessException {
+        AuthData auth = authDAO.getAuth(userData.username());
+        if (auth == null) {
+            throw new DataAccessException("User not logged in");
+        }
+        authDAO.deleteAuth(auth.authToken());
+    }
 
 }
